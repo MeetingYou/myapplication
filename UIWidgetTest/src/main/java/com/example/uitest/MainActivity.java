1e197c8e19edfdd21cbce3a22b5cd50a3818d246
MeetingYou <616013524@qq.com>
2016-05-05 23:48:51 +0800

    注意ProgressBar的Style，以及AlertDialog的用法

--- UIWidgetTest/src/main/java/com/example/uitest/MainActivity.java
+++ UIWidgetTest/src/main/java/com/example/uitest/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.uitest;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +17,7 @@ public class MainActivity extends Activity {
     private Button button;
     private EditText editText;
     private ProgressBar progressBar;
+    private Button alertButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,14 @@ public class MainActivity extends Activity {
         button.setOnClickListener(new MyOnclickListener());
         editText = (EditText)findViewById(R.id.edit_text);
         progressBar = (ProgressBar)findViewById(R.id.progress_bar);
+        alertButton = (Button)findViewById(R.id.alert_button);
+        alertButton.setOnClickListener(new MyOnclickListener());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Anhui", "onDestroy()");
     }
 
     private class MyOnclickListener implements View.OnClickListener {
@@ -42,6 +54,25 @@ public class MainActivity extends Activity {
                     } else {
                         progressBar.setVisibility(View.VISIBLE);
                     }
+                    break;
+                case R.id.alert_button:
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                    alertDialog.setTitle("Are you Rowling?");
+                    alertDialog.setMessage("Yes or no?");
+                    alertDialog.setCancelable(false);
+                    alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, "Hi Rowling", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            MainActivity.this.finish();
+                        }
+                    });
+                    alertDialog.show();
                     break;
             }
         }
