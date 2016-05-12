c594351b5b288a3d1daa61e3f865be25a3eb6d23
MeetingYou <616013524@qq.com>
2016-05-12 23:45:27 +0800

    修正布局文件，尤其学习padding和margin的区别

--- UIBestPractise/src/main/java/com/example/uibestpractise/MsgAdapter.java
+++ UIBestPractise/src/main/java/com/example/uibestpractise/MsgAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,6 +30,8 @@ public class MsgAdapter extends ArrayAdapter<Msg> {
 
         if (convertView==null) {
             view = LayoutInflater.from(getContext()).inflate(mResource, null);
+            viewHolder.mLeftLayout = (LinearLayout)view.findViewById(R.id.left_layout);
+            viewHolder.mRightLayout = (LinearLayout)view.findViewById(R.id.right_layout);
             viewHolder.mSendTextView = (TextView)view.findViewById(R.id.send_textview);
             viewHolder.mReceiveTextView = (TextView)view.findViewById(R.id.receive_textview);
             view.setTag(viewHolder);
@@ -38,18 +41,20 @@ public class MsgAdapter extends ArrayAdapter<Msg> {
         }
         if (message.getType()==Msg.TYPE_SEND) {
             viewHolder.mSendTextView.setText(message.getContent());
-            viewHolder.mSendTextView.setVisibility(View.VISIBLE);
-            viewHolder.mReceiveTextView.setVisibility(View.GONE);
+            viewHolder.mRightLayout.setVisibility(View.VISIBLE);
+            viewHolder.mLeftLayout.setVisibility(View.GONE);
         } else if (message.getType()==Msg.TYPE_RECEIVE) {
             viewHolder.mReceiveTextView.setText(message.getContent());
-            viewHolder.mSendTextView.setVisibility(View.GONE);
-            viewHolder.mReceiveTextView.setVisibility(View.VISIBLE);
+            viewHolder.mRightLayout.setVisibility(View.GONE);
+            viewHolder.mLeftLayout.setVisibility(View.VISIBLE);
         }
 
         return  view;
     }
 
     private class ViewHolder {
+        LinearLayout mLeftLayout;
+        LinearLayout mRightLayout;
         TextView mSendTextView;
         TextView mReceiveTextView;
     }
