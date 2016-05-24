2f6c214ec766b12e3afac3c2d772a654e9c31b9c
MeetingYou <616013524@qq.com>
2016-05-25 00:00:25 +0800

    添加本地广播，以及总结广播

---
+++ BroadcastTest3/src/main/java/com/example/broadcasttest3/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.broadcasttest3;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/*
+安卓发出的广播可以分为四种：
+Standard broadcast
+System broadcast
+Ordered broadcast
+Local broadcast
+
+静态注册的广播，app不启动不一定可以收到广播，原因是所有的广播默认添加了Intent.FLAG_EXCLUDE_STOPPED_PACKAGES
+FLAG_INCLUDE_STOPPED_PACKAGES：包含已经停止的包（停止：即包所在的进程已经退出）
+FLAG_EXCLUDE_STOPPED_PACKAGES：不包含已经停止的包
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private LocalBroadcastManager mLocalBroadcastManager;
+    private LocalBroadcastReceiver mLocalBroadcastReceiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
+        IntentFilter intentFilter = new IntentFilter("android.intent.action.localbroadcast");
+        mLocalBroadcastReceiver = new LocalBroadcastReceiver();
+
+        mLocalBroadcastManager.registerReceiver(mLocalBroadcastReceiver, intentFilter);
+
+        mButton = (Button)findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mLocalBroadcastManager.sendBroadcast(new Intent("android.intent.action.localbroadcast"));
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocalBroadcastManager.unregisterReceiver(mLocalBroadcastReceiver);
+    }
+
+    class LocalBroadcastReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(MainActivity.this, "Hi rowling", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
